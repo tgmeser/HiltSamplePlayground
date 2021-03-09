7ed67d9a3a04619d689531acbb3c23bfaa18dfd9
tgmeser <78986854+tgmeser@users.noreply.github.com>
2021-03-09 23:31:48 +0300

    Update Marine.kt

--- app/src/main/java/com/babyapps/hiltsample/di/model/army/Marine.kt
+++ app/src/main/java/com/babyapps/hiltsample/di/model/army/Marine.kt
@@ -3,5 +3,5 @@ package com.babyapps.hiltsample.di.model.army
 class Marine : Diver, Parachutist, FirsAider {
     override fun diveDeep(): String = "Marine is diving into the sea..!"
     override fun parachute(): String = "Marine is jumping off the plane with parachute..!"
-    override fun firstAid(): String = "Marine is giving first-aid!.."
+    override fun firstAid(): String = "Marine is giving first-aid...!"
 }
